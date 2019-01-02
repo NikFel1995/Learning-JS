be35951aa296fc842d3cc9982fa7c6ca567e50d9
NikFel <stena-55@mail.ru>
2019-01-02 18:15:26 +0500

    Начало изучения JS

---
+++ script.js
@@ -0,0 +1,30 @@
+'use strict()';
+
+
+var leftBorderWidth = 1;
+let second = 2;
+
+const pi = 3.14;
+
+//console.log("leftBorderWidth = " + leftBorderWidth);
+
+let person = {
+    name: "John",
+    age: 25,
+    isMarried: false,
+};
+
+console.log(person.name);
+
+let arr = [
+    'plum.png',
+    'orange.jpg',
+    'apple.bmp',
+];
+
+console.log(arr[2]);
+
+//alert("Hello world");
+
+//let answer = prompt("What is your name?", "Woodpecker!");
+console.log(typeof (41));
